44ccfaa0e20c2d27c547512ee11c25e59332faac
swananana <swananana@users.noreply.github.com>
2021-06-11 22:53:00 +0530

    -index-created

--- swanasimran/pages/index.jsx
+++ swanasimran/pages/index.jsx
@@ -1,8 +1,12 @@
-import react from 'React';
+/** @jsx jsx */ /** @jsxRuntime classic */ import { jsx } from "theme-ui";
+import Link from 'next/link'
+
+import theme from '../theme'
 
 export default () => {
     return (
-        <div>Hello Swana</div>
+        <div sx={{fontWeight: 'bold', fontSize: 4, cursor: 'pointer'}}
+        >Hello Swana</div>
     )
 } 
 
