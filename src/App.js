fa358925ad2eb9ad8bc54744e7cb9ee4622b8a8e
swananana <swananana@users.noreply.github.com>
2021-06-16 23:47:17 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import Login from './pages/Login'
 
 export default function App(){
     return(
-        
+        <Router>
+            <Switch>
+                <Route exact path='/' component= {Login}/>
+            </Switch>
+        </Router>
 
     )
 }
