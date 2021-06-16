2f25fdf36d2f1814d456399812e2fdd049842a4a
swananana <swananana@users.noreply.github.com>
2021-06-17 01:41:52 +0530

    login

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from 'firebase/app';
 import App from './App';
-import 'firebase/auth';
-import 'firebase/analytics';
-import 'firebase/firestore';
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCgXk0sZUW9DrnKgMDSb2qHmcMmjvfGZ_Y",
-  authDomain: "the-notebook-swana.firebaseapp.com",
-  projectId: "the-notebook-swana",
-  storageBucket: "the-notebook-swana.appspot.com",
-  messagingSenderId: "654154169796",
-  appId: "1:654154169796:web:789d61c04d5e1e018b7891",
-  measurementId: "G-FCKF1SYRJB"
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,56 @@
 import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
 
-export default function Login(){
-    return(
-        <div className="container d-flex align-items-center justify-content-center flex-column">
-            <h2 className="mb-4">
-                The Notebook
-            </h2>
-            <Button variant="contained" color = "primary" style={{textTransform : "capitalize" }}>
-                Sign In with Google
-            </Button>
+import "./firebase";
 
-        </div>
-    )
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default function Login() {
+  const history = useHistory();
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      console.log(user);
+      history.push("/home");
+    }
+  });
+
+  function signInWithGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  }
+
+  return (
+    <section>
+      <div
+        className="container d-flex align-items-center justify-content-center flex-column"
+        style={{ minHeight: "100vh" }}
+      >
+        <h1 className="text-primary">
+          <strong>reactor</strong>-pad-app
+        </h1>
+        <Button
+          variant="contained"
+          color="primary"
+          className="mt-3"
+          style={{ textTransform: "capitalize" }}
+          onClick={signInWithGoogle}
+        >
+          Sign in with Google
+        </Button>
+      </div>
+    </section>
+  );
 }
